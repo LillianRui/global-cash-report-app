4a99e837b072bbf27bd24769a74e93bba3513c59
Lillian Rui <lr1364@stern.nyu.edu>
2018-06-18 19:58:31 -0400

    wip

--- consolidator.py
+++ consolidator.py
@@ -1,13 +1,19 @@
 # consolidator.py
 
-print("PROCESSING SOME CSV FILES HERE")
-
 import csv
 import os
+import datetime
+import pandas
+
+print("""
+-------------------------------------------------------
+              Global Weekly Cash Report
+-------------------------------------------------------
+""")
+print("Produced as of "+datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
 
 
 csv_filenames = os.listdir("submissions")
-
 
 print(csv_filenames)
 
@@ -21,3 +27,20 @@ for filename in csv_filenames:
         monthly_sales = sum(all_sales)
         monthly_sales_usd = "${0:,.2f}".format(monthly_sales)
         print("... Total Sales: " + monthly_sales_usd)
+
+def mergeCsv():
+    csvx_list = glob.glob('*.csv')
+    print('here is %s CSV file'% len(csvx_list))
+    time.sleep(2)
+    print('running............')
+    for i in csvx_list:
+        fr = open(i,'r').read()
+        with open('add.csv','a') as f:
+            f.write(fr)
+        print('success')
+    print("success")
+mergeCsv()
+
+
+#TODO: how to merge multiple csv into one file
+#TODO: how to
